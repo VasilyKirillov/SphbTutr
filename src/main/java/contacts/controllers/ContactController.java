3ff2fa0c848cb26619794536ed007b4a1613b6f4
gress <gressmc@gmail.com>
2018-02-08 11:16:28 +0400

    ContantController split Post methods to postAddContact postEditContact and postDeletContact ; to make redirect after making changes in DB you need to return not only the name of the view but also a "redirect:" prefix with that

--- src/main/java/contacts/controllers/ContactController.java
+++ src/main/java/contacts/controllers/ContactController.java
@@ -18,7 +18,6 @@ import contacts.repositories.ContactRepository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class ContactController {
 
@@ -26,14 +25,14 @@ public class ContactController {
     private ContactRepository contactRepository;
 
     @RequestMapping(value = "/contacts", method = RequestMethod.GET)
-    public String getContactList(Model model){
+    public String getContactList(Model model) {
         model.addAttribute("contacts", contactRepository.findAll());
-        return "contact/list"; 
+        return "contact/list";
         /* instead of getting requestDispatcher and forward request , just return the name of the view
         in spring-servlet.xml we define InternalResourceViewResolver bean which has properties :
         prefix and suffix which added to the name of the view so it would be  /view/contact/list.jsp */
     }
-    
+
     //    @RequestMapping(value = "/contacts", method = RequestMethod.GET)
 //    public void getContactList(HttpServletRequest request, HttpServletResponse response)
 //            throws IOException, ServletException {
@@ -42,22 +41,23 @@ public class ContactController {
 //        RequestDispatcher view = request.getRequestDispatcher("view/contact/list.jsp");
 //        view.forward(request, response);
 //    }
-    
-    @RequestMapping(value = "/contact", params = "add", method = RequestMethod.GET) 
-    public String getAddContact(Model model){
+    @RequestMapping(value = "/contact", params = "add", method = RequestMethod.GET)
+    public String getAddContact(Model model) {
         return "/contact/add";
     }
-    @RequestMapping(value = "/contact", params = "edit", method = RequestMethod.GET) 
-    public String getEditContact(@RequestParam long id, Model model){
+
+    @RequestMapping(value = "/contact", params = "edit", method = RequestMethod.GET)
+    public String getEditContact(@RequestParam long id, Model model) {
         model.addAttribute("contact", contactRepository.findOne(id));
         return "/contact/edit";
     }
-    @RequestMapping(value = "/contact", method = RequestMethod.GET) 
-    public String getViewContact(@RequestParam long id, Model model){
+
+    @RequestMapping(value = "/contact", method = RequestMethod.GET)
+    public String getViewContact(@RequestParam long id, Model model) {
         model.addAttribute("contact", contactRepository.findOne(id));
         return "/contact/view";
     }
-    
+
 //    @RequestMapping(value = "/contact", method = RequestMethod.GET)
 //    protected void getContact(HttpServletRequest request, HttpServletResponse response)
 //            throws ServletException, IOException {
@@ -82,50 +82,74 @@ public class ContactController {
 //            }
 //        }
 //    }
-    
-    
-    
-    
-    @RequestMapping(value = "/contact", method = RequestMethod.POST)
-    protected void postContact(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-        if (request.getParameter("add") != null) {
-            // create new contact and address from form parameters, and
-            // persist
-            Address address = new Address(request.getParameter("street"),
-                    request.getParameter("city"),
-                    request.getParameter("state"),
-                    request.getParameter("zip"));
-            Contact contact = new Contact(request.getParameter("name"), address);
-            contact = contactRepository.save(contact);
-
-            // redirect to contact view page
-            response.sendRedirect("contact?id=" + contact.getId());
-
-        } else if (request.getParameter("edit") != null) {
-
-            // look up existing contact and address, edit fields and persist
-            long id = Long.parseLong(request.getParameter("id"));
-            Contact contact = contactRepository.findOne(id);
-            Address address = contact.getAddress();
-            contact.setName(request.getParameter("name"));
-            address.setStreet(request.getParameter("street"));
-            address.setCity(request.getParameter("city"));
-            address.setState(request.getParameter("state"));
-            address.setZip(request.getParameter("zip"));
-            contactRepository.save(contact);
-
-            // redirect to contact view page
-            response.sendRedirect("contact?id=" + contact.getId());
-        } else if (request.getParameter("delete") != null) {
-            // look up existing contact, and delete
-            long id = Long.parseLong(request.getParameter("id"));
-            Contact contact = contactRepository.findOne(id);
-            contactRepository.delete(contact);
-
-            // redirect to contact list page
-            response.sendRedirect("contacts");
-        }
+    @RequestMapping(value = "/contact", params = "add", method = RequestMethod.POST)
+    public String postAddContact(@RequestParam String name, @RequestParam String street,
+            @RequestParam String city, @RequestParam String state, @RequestParam String zip) {
+        Contact contact = new Contact(name, new Address(street, city, state, zip));
+        contact = contactRepository.save(contact);
+        return "redirect:contact?id=" + contact.getId();
     }
+
+    @RequestMapping(value = "/contact", params = "edit", method = RequestMethod.POST)
+    public String postEditContact(@RequestParam long id, @RequestParam String name, @RequestParam String street,
+            @RequestParam String city, @RequestParam String state, @RequestParam String zip) {
+        Contact contact = contactRepository.findOne(id);
+        Address address = contact.getAddress();
+        contact.setName(name);
+        address.setStreet(street);
+        address.setCity(city);
+        address.setState(state);
+        address.setZip(zip);
+        contactRepository.save(contact);
+        return "redirect:contact?id=" + contact.getId();
+    }
+
+    @RequestMapping(value = "/contact", params = "delete", method = RequestMethod.POST)
+    public String postDeleteContact(@RequestParam long id) {
+        contactRepository.delete(id);
+        return "redirect:contacts";
+    }
+
+//    @RequestMapping(value = "/contact", method = RequestMethod.POST)
+//    protected void postContact(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        
+//        if (request.getParameter("add") != null) {
+//            // create new contact and address from form parameters, and
+//            // persist
+//            Address address = new Address(request.getParameter("street"),
+//                    request.getParameter("city"),
+//                    request.getParameter("state"),
+//                    request.getParameter("zip"));
+//            Contact contact = new Contact(request.getParameter("name"), address);
+//            contact = contactRepository.save(contact);
+//
+//            // redirect to contact view page
+//            response.sendRedirect("contact?id=" + contact.getId());
+//
+//        } else if (request.getParameter("edit") != null) {
+//
+//            // look up existing contact and address, edit fields and persist
+//            long id = Long.parseLong(request.getParameter("id"));
+//            Contact contact = contactRepository.findOne(id);
+//            Address address = contact.getAddress();
+//            contact.setName(request.getParameter("name"));
+//            address.setStreet(request.getParameter("street"));
+//            address.setCity(request.getParameter("city"));
+//            address.setState(request.getParameter("state"));
+//            address.setZip(request.getParameter("zip"));
+//            contactRepository.save(contact);
+//
+//            // redirect to contact view page
+//            response.sendRedirect("contact?id=" + contact.getId());
+//        } else if (request.getParameter("delete") != null) {
+//            // look up existing contact, and delete
+//            long id = Long.parseLong(request.getParameter("id"));
+//            Contact contact = contactRepository.findOne(id);
+//            contactRepository.delete(contact);
+//
+//            // redirect to contact list page
+//            response.sendRedirect("contacts");
+//        }
+//    }
 }
